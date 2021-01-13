e9e0b3992b2fb7ef05f4ffd63faf78d3e5d8ff96
vovaZuev <vovazuev@yandex.ru>
2021-01-14 02:20:23 +0700

    templated separated DONE

---
+++ GitBasics/Functions.cpp
@@ -0,0 +1,128 @@
+#include "Header.h"
+
+
+
+template <typename T>
+void Print(T arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+
+template <typename T>
+void Print(T arr[ROWS][COLS], const int m, const int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+template <typename T>
+void Sort(T arr[], const int n)
+{
+	//Сортировка:
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				double buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+template <typename T>
+void Sort(T arr[ROWS][COLS], const int m, const int n)
+{
+#ifdef DEBUG
+	int iterations = 0;
+	int exchanges = 0;
+#endif // DEBUG
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			for (int k = i; k < m; k++)
+			{
+				/*for (;;)
+				{
+
+				}*/
+
+				int l;
+				if (k == i)l = j + 1;
+				else l = 0;
+
+				//			condition ? value1 : value2;
+				for (/*int l = (k == i) ? j + 1 : 0*/; l < n; l++)
+				{
+#ifdef DEBUG
+					iterations++;
+#endif // DEBUG
+					if (/*ПЕРЕБИРАЕМЫЙ ЭЛЕМЕН*/arr[k][l] < arr[i][j]/*ВЫБРАННЫЙ ЭЛЕМЕН*/)
+					{
+						int buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+#ifdef DEBUG
+						exchanges++;
+#endif // DEBUG
+					}
+				}
+			}
+		}
+	}
+#ifdef DEBUG
+	cout << "Массив отсортирован за " << iterations << " итераций\n";
+	cout << "При этом, было выполнено " << exchanges << " обменов элементов\n";
+#endif // DEBUG
+}
+
+template <typename T>
+T Sum(T arr[], const int n)
+{
+	T Sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
+
+template <typename T>
+T Sum(T arr[ROWS][COLS], const int m, const int n)
+{
+	T sum = 0;
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			sum += arr[i][j];
+		}
+	}
+	return sum;
+}
+
+template <typename T>
+double Avg(T arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+template <typename T>
+double Avg(T arr[ROWS][COLS], const int m, const int n)
+{
+	return (double)Sum(arr, m, n) / (m * n);
+}
---
+++ GitBasics/Header.h
@@ -0,0 +1,30 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+const int ROWS = 8;	//ROW - строка
+const int COLS = 7; //COLUMN - столбец
+
+void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+void FillRand(int arr[ROWS][COLS], const int m, const int n);
+
+template <typename T>
+void Print(T arr[], const int n);
+template <typename T>
+void Print(T arr[ROWS][COLS], const int m, const int n);
+
+template <typename T>
+void Sort(T arr[], const int n);
+template <typename T>
+void Sort(T arr[ROWS][COLS], const int m, const int n);
+
+template <typename T>
+T Sum(T arr[], const int n);
+template <typename T>
+T Sum(T arr[ROWS][COLS], const int m, const int n);
+
+template <typename T>
+double Avg(T arr[], const int n);
+template <typename T>
+double Avg(T arr[ROWS][COLS], const int m, const int n);
